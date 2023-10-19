cc6df2405ec9d746eb454ed11656e0a1219cf45e
Dan Schultz <dan@formula23.com>
2023-10-19 10:20:51 -0400

    updates

--- resources/js/Pages/Chat/Index.vue
+++ resources/js/Pages/Chat/Index.vue
@@ -222,7 +222,7 @@ onMounted(() => {
     <Head title="Chat" />
 
     <AuthenticatedLayout>
-        <template #header> Chat </template>
+        <template #header>Chats</template>
 
       <div v-if="errorMessage" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
         <span class="block sm:inline">{{ errorMessage }}</span>
