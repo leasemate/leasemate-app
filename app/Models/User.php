b94029557fc30ee9214a0aa2e6d487529c55ef53
Dan Schultz <dan@formula23.com>
2023-11-15 23:05:56 -0800

    remove email verification requirement

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,19 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+//use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
+    // removed implements MustVerifyEmail
+    // we don't want to verify email for now
+    // implements MustVerifyEmail
+
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
