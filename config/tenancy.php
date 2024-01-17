7b1905643dcb38ad27216bbcb09f879bdc7ee52f
Dan Schultz <dan@formula23.com>
2024-01-16 20:33:14 -0800

    update central domain variable

--- config/tenancy.php
+++ config/tenancy.php
@@ -17,9 +17,7 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        'reai.test',
-        'reai-dev.com',
-//        'reai.us-1.sharedwithexpose.com',
+        env('APP_URL'),
     ],
 
     /**
