1c5757d75378d08952ad37ca487dd55c1a22080f
Dan Schultz <dan@formula23.com>
2024-01-17 09:42:44 -0800

    update api token factory

--- database/factories/ApiTokenFactory.php
+++ database/factories/ApiTokenFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ApiToken>
@@ -17,7 +18,7 @@ class ApiTokenFactory extends Factory
     public function definition(): array
     {
         return [
-            'api_token' => hash('sha256', $this->faker->unique()->uuid()),
+            'api_token' => Str::uuid()->toString(),
         ];
     }
 }
