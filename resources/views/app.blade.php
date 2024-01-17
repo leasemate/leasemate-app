51d9168481464ad916816b2f48270d8f5da28fbf
Dan Schultz <dan@formula23.com>
2024-01-16 23:14:12 -0800

    update meta title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,7 @@
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         <meta
-            content="Tailwind Admin & Dashboard Template"
+            content="LeaseMate - Property Management Software"
             name="description"
         />
 
