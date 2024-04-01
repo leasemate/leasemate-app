916367708108083b116feb767cdf9d7f484064a6
Dan Schultz <dan@formula23.com>
2024-03-31 20:26:20 -0700

    updates

--- public/build/assets/BoxIcon-3beb6c8b.js
+++
@@ -1,1 +0,0 @@
-import{o as e,d as o,n as a}from"./app-73731c72.js";const l={__name:"BoxIcon",props:{class:{type:String,default:"button"}},setup(s){const t=s;return(n,c)=>(e(),o("i",{class:a(["bx text-[20px]",t.class])},null,2))}};export{l as _};
