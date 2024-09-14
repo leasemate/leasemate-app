92e98ed5c0ea2311eefb4a01adb69747a92017d3
Dan Schultz <dan@formula23.com>
2024-09-14 14:40:13 -0700

    fix admin

--- resources/js/Pages/Landlord/Tenant/Index.vue
+++ resources/js/Pages/Landlord/Tenant/Index.vue
@@ -1,32 +1,31 @@
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
-import { defineProps } from 'vue';
-import SecondaryButton from "@/Components/SecondaryButton.vue";
-import DangerButton from "@/Components/DangerButton.vue";
-import { Link, router } from '@inertiajs/vue3';
-import PrimaryButton from "@/Components/PrimaryButton.vue";
-import PrimaryLink from "@/Components/PrimaryLink.vue";
+import AppLayout from '@/Layouts/AppLayout.vue'
+import { defineProps } from 'vue'
+import SecondaryButton from '@/Components/SecondaryButton.vue'
+import DangerButton from '@/Components/DangerButton.vue'
+import { router } from '@inertiajs/vue3'
+import PrimaryLink from '@/Components/PrimaryLink.vue'
 
 defineProps({
     tenants: {
         type: Object,
     },
-});
+})
 
 const deleteTenant = (tenant) => {
 
-    if(confirm('Are you sure you want to delete this tenant?') === false) return;
+    if (confirm('Are you sure you want to delete this tenant?') === false) return
 
     router.delete(route('tenants.destroy', tenant), {
-            preserveState: false,
-        });
+        preserveState: false,
+    })
 
 }
 
 const forceLoginTenant = (tenant) => {
     router.post(route('tenants.force-login', tenant), {
         preserveState: false,
-    });
+    })
 }
 
 </script>
@@ -44,7 +43,7 @@ const forceLoginTenant = (tenant) => {
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-4">
 
                 <div class="flex justify-end">
-                    <PrimaryLink as="button" class="" :href="route('tenants.create')">
+                    <PrimaryLink :href="route('tenants.create')" as="button" class="">
                         Create Tenant
                     </PrimaryLink>
                 </div>
@@ -55,27 +54,29 @@ const forceLoginTenant = (tenant) => {
                         <table class="w-full text-sm text-left text-gray-500 ">
                             <thead class="text-sm text-gray-700 dark:text-gray-100 bg-gray-50 dark:bg-zinc-600">
                             <tr>
-                                <th scope="col" class="px-6 py-3">
+                                <th class="px-6 py-3" scope="col">
                                     #
                                 </th>
-                                <th scope="col" class="px-6 py-3">
+                                <th class="px-6 py-3" scope="col">
                                     Domain
                                 </th>
-                                <th scope="col" class="px-6 py-3">
+                                <th class="px-6 py-3" scope="col">
                                     Name
                                 </th>
-                                <th scope="col" class="px-6 py-3">
+                                <th class="px-6 py-3" scope="col">
                                     Email
                                 </th>
-                                <th scope="col" class="px-6 py-3">
+                                <th class="px-6 py-3" scope="col">
                                     Login
                                 </th>
-                                <th></th>
+                                <th class="px-6 py-3" scope="col"></th>
                             </tr>
                             </thead>
                             <tbody>
-                            <tr v-for="tenant in tenants" class="bg-white border-b border-gray-50 dark:bg-zinc-700 dark:border-zinc-600">
-                                <th scope="row" class="px-6 py-3.5 font-medium text-gray-900 whitespace-nowrap dark:text-zinc-100">
+                            <tr v-for="tenant in tenants"
+                                class="bg-white border-b border-gray-50 dark:bg-zinc-700 dark:border-zinc-600">
+                                <th class="px-6 py-3.5 font-medium text-gray-900 whitespace-nowrap dark:text-zinc-100"
+                                    scope="row">
                                     {{ tenant.id }}
                                 </th>
                                 <td class="px-6 py-3.5 dark:text-zinc-100">
@@ -95,13 +96,10 @@ const forceLoginTenant = (tenant) => {
                                         Login
                                     </SecondaryButton>
                                 </td>
-                                <td class="px-6 py-3.5 dark:text-zinc-100 flex justify-end">
-
+                                <td class="px-6 py-3.5 dark:text-zinc-100">
                                     <DangerButton class="justify-end" @click="deleteTenant(tenant)">
                                         Delete
                                     </DangerButton>
-
-
                                 </td>
                             </tr>
 
